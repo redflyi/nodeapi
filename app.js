8676a1de9aaf12944ed25efb2d0e1963f41bf133
redflyi <ikem007@hotmail.com>
2021-04-14 17:29:42 +0100

    futher api

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true}).then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB Connection Error: ${err.message}`);
+});
+
 
 const postRoutes = require("./routes/post");
 
@@ -12,11 +23,11 @@ const postRoutes = require("./routes/post");
 // };
 
 app.use(morgan('dev'));
-
+app.use(bodyParser.json());
 // app.use(myOwnMiddleWare);
 
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {console.log(`A Node JS API is listening on port: ${port}`)});
--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,25 @@
+const Post = require('../models/post');
+
 exports.getPosts = (req, res) => {
-    res.send("Hello world");
+    res.json({
+        posts: [
+            {title: 'First Post'},
+            {title: 'Second Post'}
+        ]
+    });
+    };
+
+    exports.createPosts = (req, res) => {
+        const post = new Post(req.body);
+        //console.log("Creating Post:", req.body);
+        post.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.status(200).json({
+                post:result
+            });
+        });
     };
