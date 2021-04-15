243854f7c164441e03d434768aef528c26375544
redflyi <ikem007@hotmail.com>
2021-04-15 16:41:44 +0100

    here we go again 4

---
+++ routes/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const {signup} = require('../controllers/auth');
+const {userSignupValidator} = require('../validator');
+
+const router = express.Router();
+
+//router.get('/', getPosts);
+router.post('/signup', userSignupValidator, signup);
+
+module.exports = router;
